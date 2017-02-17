609c0c71df0eb7ee5d22161dd94b060cef3e7ca3
Andy McKay <amckay@mozilla.com>
2017-02-17 14:41:56 -0800

    step-four

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
 function closeDuplicates() {
   let found = [];
+  let closed = 0;
 
   browser.tabs.query({})
   .then((tabs) => {
     for (let tab of tabs) {
       if (found.includes(tab.url)) {
         browser.tabs.remove(tab.id);
+        closed++;
       } else {
         found.push(tab.url);
       }
     }
+  })
+  .then(() => {
+    browser.notifications.create(
+      'remove-duplicates', {
+        'message': `Number of tabs closed: ${closed}.`,
+        'title': 'Close Duplicates',
+        'type': 'basic'
+    });
   });
 }
 
